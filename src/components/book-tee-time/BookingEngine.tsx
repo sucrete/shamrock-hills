61885514fea5ecb9914f9b26b77462858b223f99
Will Andree <williamandree@gmail.com>
2026-09-14 13:11:43 -0500

    Updated booking engine.

--- src/components/book-tee-time/BookingEngine.tsx
+++ src/components/book-tee-time/BookingEngine.tsx
@@ -8,7 +8,7 @@ const BookingEngine = () => {
       <div className="main-container py-[2rem] md:py-[10rem]">
         <RevealAnimation delay={0.3}>
           <iframe
-            src="https://bookateetime.teequest.com/course/129?selectedHoles=9&walking=true"
+            src="https://bookateetime.teequest.com/course/9"
             style={{ width: '100%', minHeight: '1000px', border: 'none' }}></iframe>
         </RevealAnimation>
       </div>
