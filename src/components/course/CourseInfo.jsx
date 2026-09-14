cdace2e40ff4e674d8cb82067294f5cd10b167e9
Will Andree <williamandree@gmail.com>
2026-09-14 11:08:21 -0500

    Making final tweaks to tyling.

--- src/components/course/CourseInfo.jsx
+++ src/components/course/CourseInfo.jsx
@@ -24,7 +24,7 @@ const CourseInfo = async () => {
     <section className="bg-[#f8feda] before:content-[''] before:h-full before:w-[20%] before:max-md:hidden before:bg-[linear-gradient(to_right,#fafafa_50%,transparent)] before:absolute before:inset-0 after:content-[''] after:h-full after:w-[20%] after:max-md:hidden after:bg-[linear-gradient(to_right,#fafafa_50%,transparent)] after:absolute after:rotate-180 after:right-0 after:top-0">
       <div className="main-container py-10 sm:py-7 ">
         <div className="max-w-[950px] grid grid-cols-3 max-md:space-y-5 mx-auto">
-          <div className="col-span-12 sm:col-span-1 md:border-r border-[#02020228]">
+          <div className="col-span-12 sm:col-span-1 md:border-r border-[#dbf45c]">
             <RevealAnimation delay={0.3} offset={10}>
               <div className="text-center monospaced text-black/60 pb-1">weather</div>
               <div className="text-center weather max-md:text-[14px]">
@@ -32,7 +32,7 @@ const CourseInfo = async () => {
               </div>
             </RevealAnimation>
           </div>
-          <div className="col-span-12 sm:col-span-1  md:border-r border-[#02020228]">
+          <div className="col-span-12 sm:col-span-1  md:border-r border-[#dbf45c]">
             <RevealAnimation delay={0.4} offset={10}>
               <div className="text-center monospaced text-black/60 pb-1">date</div>
               <div className="text-center max-md:text-[14px]">
